4e77703a32324b18ceb08257f06a0fc7536d2a76
Kirtan Dusi <67548970+Kirtandusi@users.noreply.github.com>
2024-02-24 10:26:26 -0600

    more backend

--- server/server.py
+++ server/server.py
@@ -3,20 +3,25 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 def generate_arrays():
+    locations = []
     quality = []
     safety = []
     employability = []
-    return quality, safety, employability
+    prices = []
+
+    return prices, safety, employability, locations, quality
 
 
 # API endpoint to get the arrays
 @app.route("/area_data", methods=["GET"])
 def get_school_data():
-    quality, safety, employability = generate_arrays()
+    locations, quality, safety, prices, employability = generate_arrays()
     data = {
+        "locations": locations,
         "quality": quality,
         "safety": safety,
-        "employability": employability
+        "employability": employability,
+        "prices": prices,
     }
     return jsonify(data)
 
