dc420c1a155462eddef67738bb6bd17573bf62da
Ignas Dauksys <ignasdauksys@protonmail.com>
2024-02-24 12:54:39 -0600

    map making

--- housing-helper/src/wisconsin map/WisconsinMapMain.jsx
+++ housing-helper/src/wisconsin map/WisconsinMapMain.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
+import ReactMapGL from "react-map-gl";
 
 const WisconsinMapMain = () => {
-  return <div className="w-screen h-screen bg-slate-100">WisconsinMapMain</div>;
+  const [viewport, setViewport] = useState({
+    latitude: 43.784439,
+    longitude:-88.787865,
+    zoom: 6
+  });
+
+
+  return <div>
+    <ReactMapGL {...viewport}  width="100%" height="100%"
+      mapStyle="mapbox://styles/ignasd/clt0fp2za00ee01qf2xxfgflu"
+      mapboxAccessToken={"pk.eyJ1IjoiaWduYXNkIiwiYSI6ImNsdDBkb3dxczB4em4yb21uZ2hmdDZ3aXoifQ.pbbOUb_r8sNLbwrJPNmlkw"} 
+      onViewportChange={viewport => {
+        setViewport(viewport);
+      }}
+      />
+        markers here
+      
+
+  </div>
 };
 
 export default WisconsinMapMain;
