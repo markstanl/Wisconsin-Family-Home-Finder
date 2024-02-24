fb35178305ed45afae795a7fa100156c1d704bb8
Ignas Dauksys <ignasdauksys@protonmail.com>
2024-02-24 14:09:18 -0600

    please push

--- housing-helper/src/wisconsin map/WisconsinMapMain.jsx
+++ housing-helper/src/wisconsin map/WisconsinMapMain.jsx
@@ -5,17 +5,19 @@ const WisconsinMapMain = () => {
   const [viewport, setViewport] = useState({
     latitude: 43.784439,
     longitude:-88.787865,
-    height: "50vh",
-    width: "50wh",
+    height: "100%",
+    width: "100%",
     zoom: 6
   });
+
 
 
   return <div>
     <ReactMapGL {...viewport}  width="100%" height="100%"
       mapStyle="mapbox://styles/ignasd/clt0fp2za00ee01qf2xxfgflu"
       mapboxAccessToken={"pk.eyJ1IjoiaWduYXNkIiwiYSI6ImNsdDBkb3dxczB4em4yb21uZ2hmdDZ3aXoifQ.pbbOUb_r8sNLbwrJPNmlkw"} 
-      onViewportChange={viewport => (setViewport(viewport))}
+      onViewportChange={setViewport}
+      dragPan={true}
       />
         markers here
       
