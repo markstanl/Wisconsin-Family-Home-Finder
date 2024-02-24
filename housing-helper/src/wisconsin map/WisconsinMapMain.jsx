febd81aa04068720510facd7cc51172ca0f2e588
Ignas Dauksys <ignasdauksys@protonmail.com>
2024-02-24 14:05:11 -0600

    added mapbox

--- housing-helper/src/wisconsin map/WisconsinMapMain.jsx
+++ housing-helper/src/wisconsin map/WisconsinMapMain.jsx
@@ -5,6 +5,8 @@ const WisconsinMapMain = () => {
   const [viewport, setViewport] = useState({
     latitude: 43.784439,
     longitude:-88.787865,
+    height: "50vh",
+    width: "50wh",
     zoom: 6
   });
 
@@ -13,9 +15,7 @@ const WisconsinMapMain = () => {
     <ReactMapGL {...viewport}  width="100%" height="100%"
       mapStyle="mapbox://styles/ignasd/clt0fp2za00ee01qf2xxfgflu"
       mapboxAccessToken={"pk.eyJ1IjoiaWduYXNkIiwiYSI6ImNsdDBkb3dxczB4em4yb21uZ2hmdDZ3aXoifQ.pbbOUb_r8sNLbwrJPNmlkw"} 
-      onViewportChange={viewport => {
-        setViewport(viewport);
-      }}
+      onViewportChange={viewport => (setViewport(viewport))}
       />
         markers here
       
