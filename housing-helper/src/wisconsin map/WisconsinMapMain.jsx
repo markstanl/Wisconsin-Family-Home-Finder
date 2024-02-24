c0b1685618011b19e282ae2b962b5b8c5fab349b
markstanl <markgstanley1@gmail.com>
2024-02-24 14:50:53 -0600

    JOHN

--- housing-helper/src/wisconsin map/WisconsinMapMain.jsx
+++ housing-helper/src/wisconsin map/WisconsinMapMain.jsx
@@ -1,28 +1,31 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import ReactMapGL from "react-map-gl";
 
 const WisconsinMapMain = () => {
   const [viewport, setViewport] = useState({
     latitude: 43.784439,
-    longitude:-88.787865,
+    longitude: -88.787865,
     height: "100%",
     width: "100%",
-    zoom: 6
+    zoom: 6,
   });
 
-
-
-  return <div>
-    <ReactMapGL {...viewport}  width="100%" height="100%"
-      mapStyle="mapbox://styles/ignasd/clt0fp2za00ee01qf2xxfgflu"
-      mapboxAccessToken={"pk.eyJ1IjoiaWduYXNkIiwiYSI6ImNsdDBkb3dxczB4em4yb21uZ2hmdDZ3aXoifQ.pbbOUb_r8sNLbwrJPNmlkw"} 
-      onViewportChange={setViewport}
-      dragPan={true}
+  return (
+    <div>
+      <ReactMapGL
+        {...viewport}
+        width="100%"
+        height="100%"
+        mapStyle="mapbox://styles/ignasd/clt0fp2za00ee01qf2xxfgflu"
+        mapboxAccessToken={
+          "pk.eyJ1IjoiaWduYXNkIiwiYSI6ImNsdDBkb3dxczB4em4yb21uZ2hmdDZ3aXoifQ.pbbOUb_r8sNLbwrJPNmlkw"
+        }
+        onViewportChange={setViewport}
+        dragPan={true}
       />
-        markers here
-      
-
-  </div>
+      markers here
+    </div>
+  );
 };
 
 export default WisconsinMapMain;
